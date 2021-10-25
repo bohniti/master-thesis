68ef4af86cf1308aee125d23842932966ad00f6f
Timo Bohnstedt <timo.bohnstedt@gmail.com>
2021-10-25 23:23:43 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Use deep learning and computer vision techniques for historical fragment matching.',
+    author='Friedrich Alexander University ',
+    license='BSD-3',
+)
